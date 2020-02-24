5e408d40aff12540b9c8d135753c5da177362265
Doctorados <Doctorados@users.noreply.github.com>
2020-02-24 13:13:56 +0100

    day and rations now updating

--- RPG/Form1.vb
+++ RPG/Form1.vb
@@ -1,13 +1,15 @@
 ﻿Public Class Form1
-    Dim rs As ADODB.Recordset
     Dim conn As ADODB.Connection
-    Dim foodDist(3) As Boolean
-
-    Private Sub updateFields()
-
+    Dim foodDist(3) As Boolean 'array determines who gets rations
+    Dim dayCountVal As Int16
+    Dim rationCountVal As Int16
+    Private Sub Class_Initialize()
+        dayCountVal = 1
+        foodDist = {False, False, False}
+    End Sub
+    Private Sub updateFields() 'update the main menu fields
+        Dim rs As New ADODB.Recordset
         Try
-            'conn = New ADODB.Connection
-            'conn.Open("Provider=Microsoft.ACE.OLEDB.12.0;" & "Data Source=C:\Users\Damion\Google Drive\Studium\4. Semester\Informatik\main.accdb")
             rs.Open("SELECT * FROM [Character]", conn,
                     ADODB.CursorTypeEnum.adOpenStatic,
                     ADODB.LockTypeEnum.adLockPessimistic
@@ -32,14 +34,20 @@
             End If
             rs.Close()
             'update Day and Rations
+
             rs.Open("SELECT * FROM [Day]", conn,
                     ADODB.CursorTypeEnum.adOpenStatic,
                     ADODB.LockTypeEnum.adLockPessimistic
             )
             rs.MoveLast()
-            dayCounter.Text = "Day " & CStr(rs.Fields("ID").Value)
-            foodCounter.Text = "Rations: " & CStr(rs.Fields("rations").Value)
+            dayCountVal = rs.Fields("counter").Value
+            rationCountVal = rs.Fields("rations").Value
+            dayCounter.Text = "Day " & CStr(dayCountVal)
+            foodCounter.Text = "Rations: " & CStr(rationCountVal)
             rs.Close()
+            hungerBar0.ForeColor = Color.White
+            hungerBar1.ForeColor = Color.White
+            hungerBar2.ForeColor = Color.White
         Catch ex As Exception
             MsgBox("Exception: " & ex.Message)
         End Try
@@ -49,10 +57,11 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn = New ADODB.Connection
+        conn.Open("Provider=Microsoft.ACE.OLEDB.12.0;" & "Data Source=C:\Users\Damion\Google Drive\Studium\4. Semester\Informatik\main.accdb")
+
+        Dim rs As New ADODB.Recordset
         Try
-            conn = New ADODB.Connection
-            conn.Open("Provider=Microsoft.ACE.OLEDB.12.0;" & "Data Source=C:\Users\Damion\Google Drive\Studium\4. Semester\Informatik\main.accdb")
-            rs = New ADODB.Recordset
             'get possible Tasks
             rs.Open("SELECT * FROM [Task]", conn,
                     ADODB.CursorTypeEnum.adOpenStatic,
@@ -111,13 +120,25 @@
     End Function
 
     Private Sub calcDay()
+        Dim rs As New ADODB.Recordset
         Call updateHunger()
         Call updateHealth()
+        rs.Open("SELECT * FROM [Day]", conn,
+                   ADODB.CursorTypeEnum.adOpenKeyset,
+                   ADODB.LockTypeEnum.adLockOptimistic
+           )
 
+        rs.AddNew()
+        rs.Fields("rations").Value = calcRations()
+        rs.Fields("counter").Value = (dayCountVal + 1)
+        rs.Update()
+        rs.Close()
+        foodDist = {False, False, False}
         Call updateFields()
     End Sub
 
     Private Sub updateHunger()
+        Dim rs As New ADODB.Recordset
         rs.Open("SELECT hunger FROM [Character]", conn,
                    ADODB.CursorTypeEnum.adOpenStatic,
                    ADODB.LockTypeEnum.adLockPessimistic
@@ -141,6 +162,7 @@
         rs.Close()
     End Sub
     Private Sub updateHealth()
+        Dim rs As New ADODB.Recordset
         rs.Open("SELECT hunger, health FROM [Character]", conn,
                    ADODB.CursorTypeEnum.adOpenStatic,
                    ADODB.LockTypeEnum.adLockPessimistic
@@ -171,11 +193,41 @@
     End Sub
 
     Private Sub hungerBar_click_helper(ByVal i, ByRef sender)
-        foodDist(i) = Not foodDist(i)
-        If foodDist(i) Then
-            sender.ForeColor = Color.Lime
-        Else
-            sender.ForeColor = Color.White
+        If sumArray(foodDist) <= rationCountVal And Not foodDist(i) Then
+            foodDist(i) = Not foodDist(i)
+            If foodDist(i) Then
+                sender.ForeColor = Color.Lime
+            Else
+                sender.ForeColor = Color.White
+            End If
         End If
     End Sub
+    Function calcRations()
+        Dim rs As New ADODB.Recordset
+        Dim num As Int16
+        num = 0
+        For Each elem In foodDist
+            If elem Then
+                num += 1
+            End If
+        Next
+        rs.Open("SELECT rations FROM [Day]", conn,
+                    ADODB.CursorTypeEnum.adOpenStatic,
+                    ADODB.LockTypeEnum.adLockPessimistic
+            )
+        rs.MoveLast()
+        calcRations = (rs.Fields("rations").Value - num)
+        rs.Close()
+    End Function
+
+    Function sumArray(ByVal arr)
+        Dim num As Int16
+        For Each elem In arr
+            If elem Then
+                num += 1
+            End If
+        Next
+        MsgBox(num)
+        sumArray = num
+    End Function
 End Class
